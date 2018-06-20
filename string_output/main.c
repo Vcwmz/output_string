d4cfc6c2d8570533018c5e3e68310438a87e0861
Vcwmz <32869766+Vcwmz@users.noreply.github.com>
2018-06-20 16:48:48 +0800

    firstly
    实现

---
+++ string_output/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int len(char a[])
+{
+    int i=0;
+    while(a[i]!='\0')
+        i++;
+    return i;
+}
+void reverse(char a[],char b[])
+{
+    int i;
+    for(i=len(a);i>0;i--)
+    {
+    b[len(a)-i]=a[i-1];
+    }
+    b[len(a)]='\0';
+}
+int main()
+{
+    char a[512],b[512];
+    printf("please input the string you want to reverse output\n");
+    scanf("%s",a);
+    reverse(a,b);
+    printf(b);
+    return 0;
+
+}
